e8ec9c4f4cd901501f9a52cafd22cc7edd72d509
victor <victorlopes9291@gmail.com>
2023-12-16 08:59:12 -0300

    #1 - Criação do Clipper efeito de onda

---
+++ lib/widgets/WaveClipperBottom.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/widgets.dart';
+
+class WaveClipperBottom extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(0, 30);
+
+    path.quadraticBezierTo(size.width / 7, 0, size.width / 30, 30);
+    path.quadraticBezierTo(size.width / 1.5, size.height / 2, size.width, size.height / 2 -30);
+    path.lineTo(size.width, size.width);
+    path.lineTo(0, size.height);
+    
+    return path;
+
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+  
+}
