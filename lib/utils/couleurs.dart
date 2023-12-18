41092829ed2840c875d8b7d7c46f673c7f84fa72
victor <victorlopes9291@gmail.com>
2023-12-17 21:25:20 -0300

    #2 - Palette de couleurs

---
+++ lib/utils/couleurs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Couleurs {
+
+  static const Color primaryColor = Color.fromRGBO(134, 29, 29, 1);
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color backgroundColor = Color(0xFFF1EFF1);
+  
+  static const Color greyDark = Color.fromRGBO(67, 67, 67, 1);
+  static const Color greyMedium = Color.fromRGBO(133, 135, 139, 1);
+  static const Color grey100 = Color.fromRGBO(14, 14, 14, 1);
+  static const Color grey200 = Color.fromRGBO(26, 26, 26, 1);
+  static const Color greyLight = Color(0xFFd8d9da);
+}
