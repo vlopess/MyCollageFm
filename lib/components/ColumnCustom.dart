b48dccadf5a87b4e599543d02f42df2183e57cf6
victor <victorlopes9291@gmail.com>
2023-12-18 23:23:49 -0300

    #3 - Criando uma coluna personalizada

---
+++ lib/components/ColumnCustom.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:my_collage_fm/utils/couleurs.dart';
+
+class ColumnCustom extends StatelessWidget {
+  final String label;  
+  final String title;
+
+  const ColumnCustom({super.key, required this.label, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(        
+        children: [
+          Text(label, style: const TextStyle(fontFamily: "Barlow", fontWeight: FontWeight.bold, fontSize: 17, color: Couleurs.primaryColor)),                        
+          Text(title, style: const TextStyle(fontFamily: "Barlow", color: Couleurs.white, fontSize: 13)),
+        ],
+      ),
+    );
+  }
+}
