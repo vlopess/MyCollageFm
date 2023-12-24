29c03907cf57b9a5a26d2bf3bf6465fe8d2c7130
victor <victorlopes9291@gmail.com>
2023-12-24 16:41:14 -0300

    #4 [REF] - Salvando os dados básicos do user

--- lib/service/prefs_service.dart
+++ lib/service/prefs_service.dart
@@ -1,10 +1,13 @@
+import 'package:my_collage_fm/models/user.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreference {
 
-  static save(String userName) async {
+  static save(User user) async {
     var prefs = await SharedPreferences.getInstance();
-    prefs.setString('Username', userName);
+    prefs.setString('Username', user.name!);
+    prefs.setString('UserImage', user.image!);
+    prefs.setString('UserUrl', user.url!);
 
   }
 
@@ -14,9 +17,23 @@ class SharedPreference {
     return userName;
   }
 
+  static getUserImage() async {
+    var prefs = await SharedPreferences.getInstance();
+    var userImage = prefs.getString('UserImage');
+    return userImage;
+  }
+
+  static getUserUrl() async {
+    var prefs = await SharedPreferences.getInstance();
+    var userUrl = prefs.getString('UserUrl');
+    return userUrl;
+  }
+
   static remove() async{
     var prefs = await SharedPreferences.getInstance();
     await prefs.remove('Username');
+    await prefs.remove('UserImage');
+    await prefs.remove('UserUrl');
 
   }
 }
