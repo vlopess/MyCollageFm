ea137fa2fd1a31baca67bcd5cb4d7c2e29aa277a
victor <victorlopes9291@gmail.com>
2023-12-24 16:39:46 -0300

    #4 [REF] - Ajustes na repetição do gif de Done

--- lib/widgets/Done.dart
+++ lib/widgets/Done.dart
@@ -10,6 +10,6 @@ class Done extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  return SizedBox(width: 75, child: LottieBuilder.asset("assets/json/done.json"));
+  return SizedBox(width: 75, child: LottieBuilder.asset("assets/json/done.json",repeat: false));
   }
 }
