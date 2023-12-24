5bf377e726000330d4c2c2130ca7f57cbf842cde
victor <victorlopes9291@gmail.com>
2023-12-24 16:42:02 -0300

    #4 [REF] - Modularizando os métodos em ApiService

--- lib/service/apiService.dart
+++ lib/service/apiService.dart
@@ -9,114 +9,136 @@ import 'package:my_collage_fm/models/lovedtracks.dart';
 import 'package:my_collage_fm/models/track.dart';
 import 'package:my_collage_fm/models/user.dart';
 import 'package:my_collage_fm/service/prefs_service.dart';
+import 'package:my_collage_fm/utils/apiurl.dart';
 
 
 class ApiService {
   static const apiKey = "53ca750ff08680650a1aa431bf02a97a";
-
-  static Future<User> getAllInfoUser(String userName) async {
-    try {
-        //https://ws.audioscrobbler.com/2.0/?method=track.getinfo&artist=misfits&track=helena&api_key=53ca750ff08680650a1aa431bf02a97a&format=json
-        User user = User();
-        // 1) Pega os dados básicos do user [x]
-        Uri url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=$userName&api_key=$apiKey&format=json");
-        var responsed = await http.get(url);        
-        if (responsed.statusCode == 200) {
-          var list = jsonDecode(responsed.body);
-          user = User.fromJson(list['user']);
-        }
-        // 2) Pega as faixas preferidas do user [x]
-        url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=$userName&api_key=$apiKey&limit=10&format=json");
-        responsed = await http.get(url);
-        if (responsed.statusCode == 200) {
-          var list = jsonDecode(responsed.body);
-          user.lovedtracks = Lovedtracks.fromJson(list['lovedtracks']);          
-          for (var e in user.lovedtracks.track!) {
-            url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=track.getinfo&artist=${e.artist}&track=${e.name}&api_key=53ca750ff08680650a1aa431bf02a97a&format=json");
-            responsed = await http.get(url);
-            if(responsed.statusCode == 200){
-              var list = jsonDecode(responsed.body);
-              if(list['error'] == null){                           
-                if(list['track']['album'] != null){
-                  var realImage = list['track']['album']['image'][2]['#text'];
-                  e.image = realImage;
-                } 
-              }
-            }
-          }
-        }
-        // 3) Pega as faixas recentes do user [ok]
-        url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$userName&api_key=$apiKey&limit=1&format=json");
-        responsed = await http.get(url);
-        if (responsed.statusCode == 200) {
-          var list = jsonDecode(responsed.body);
-          list = list['recenttracks'];
-          user.recentTrack = List<Track>.from((list['track'] as List<dynamic>).map<Track>((x) => Track.fromJson(x as Map<String, dynamic>))).first;
-        }
-        // 4) Pega top faixas do user [ok]
-        url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=$userName&api_key=$apiKey&limit=5&format=json");
-        responsed = await http.get(url);
-        if (responsed.statusCode == 200) {
-          var list = jsonDecode(responsed.body);
-          list = list['toptracks'];
-          user.toptracks = List<Track>.from((list['track'] as List<dynamic>).map<Track>((x) => Track.fromJson(x as Map<String, dynamic>)));
-          for (var e in user.toptracks) {            
-            url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=track.getinfo&artist=${e.artist}&track=${e.name}&api_key=53ca750ff08680650a1aa431bf02a97a&format=json");
-            responsed = await http.get(url);
-            if(responsed.statusCode == 200){
-              var list = jsonDecode(responsed.body);
-              if(list['error'] == null){
-                if(list['track']['album'] != null){
-                  var realImage = list['track']['album']['image'][2]['#text'];
-                  e.image = realImage;
-                }else{
-                  url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${e.artist}&api_key=53ca750ff08680650a1aa431bf02a97a&format=json");
-                  responsed = await http.get(url);
-                  if(responsed.statusCode == 200){
-                    var list = jsonDecode(responsed.body);
-                    if(list['topalbums'] != null){
-                      var realImage = list['topalbums']['album'][0]['image'][3]['#text'];
-                      e.image = realImage;
-                    }
-                  }
-                }
-              }              
-            }
-          }
-        }
-        // 4) Pega top artista do user [ok]
-        url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=$userName&api_key=$apiKey&limit=5&format=json");
-        responsed = await http.get(url);
-        if (responsed.statusCode == 200) {
-          var list = jsonDecode(responsed.body);
-          list = list['topartists'];
-          user.topartists = List<Artist>.from((list['artist'] as List<dynamic>).map<Artist>((x) => Artist.fromJson(x as Map<String, dynamic>)));
-          for (var e in user.topartists!) {
-            url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${e.name}&api_key=53ca750ff08680650a1aa431bf02a97a&format=json");
-            responsed = await http.get(url);
-            if(responsed.statusCode == 200){
-              var list = jsonDecode(responsed.body);
-              if(list['topalbums']['album'] != null){
-                var realImage = list['topalbums']['album'][0]['image'][3]['#text'];
-                e.image = realImage;
-              }
-            }
-          }
-        }
-        // 4) Pega top albuns do user [ok]
-        url = Uri.parse("https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=$userName&api_key=$apiKey&limit=5&format=json");
-        responsed = await http.get(url);
-        if (responsed.statusCode == 200) {
-          var list = jsonDecode(responsed.body);
-          list = list['topalbums'];
-          user.topalbums = List<Album>.from((list['album'] as List<dynamic>).map<Album>((x) => Album.fromJson(x as Map<String, dynamic>)));
-        }        
-        SharedPreference.save(userName);
-        return user;
-      
-    } catch (ex) {
-      rethrow;
+  
+  static Future<User> findUserByUsername(String userName) async{
+    User user = User();
+    // 1) Pega os dados básicos do user [x]
+    Uri url = Uri.parse(ApiUrl.buildUrl(Method.getinfoUser(userName), apiKey));
+    var responsed = await http.get(url);        
+    if (responsed.statusCode == 200) {
+      var list = jsonDecode(responsed.body);
+      user = User.fromJson(list['user']);
     }
+    if(user.isNull()) throw Exception();
+    return user;
+  }
+
+  static Future<Lovedtracks> getlovedtracksUser() async {
+    Lovedtracks lovedtracks = Lovedtracks();
+    var userName = await SharedPreference.getUsername();
+    Uri url = Uri.parse(ApiUrl.buildUrl(Method.getlovedtracksUser(userName), apiKey));
+    var responsed = await http.get(url);
+    if (responsed.statusCode == 200) {
+      var list = jsonDecode(responsed.body);
+      lovedtracks = Lovedtracks.fromJson(list['lovedtracks']);          
+      for (var e in lovedtracks.track!) {
+        url = Uri.parse(ApiUrl.buildUrl(Method.getinfoTrack(e.artist!, e.name!), apiKey));
+        responsed = await http.get(url);
+        if(responsed.statusCode == 200){
+          var list = jsonDecode(responsed.body);
+          if(list['error'] == null){                           
+            if(list['track']['album'] != null){
+              var realImage = list['track']['album']['image'][2]['#text'];
+              e.image = realImage;
+            } 
+          }
+        }
+      }
+    }
+    return lovedtracks;
+  }
+
+  static Future<Track?> getrecenttracksUser() async{
+    var userName = await SharedPreference.getUsername();
+    var url = Uri.parse(ApiUrl.buildUrl(Method.getrecenttracksUser(userName), apiKey));
+    var responsed = await http.get(url);
+    Track recentTrack = Track();
+    if (responsed.statusCode == 200) {
+      var list = jsonDecode(responsed.body);
+      list = list['recenttracks'];
+      if(list['track'].isNotEmpty){
+          recentTrack = List<Track>.from((list['track'] as List<dynamic>).map<Track>((x) => Track.fromJson(x as Map<String, dynamic>))).first;
+      }          
+    }
+    return recentTrack;
+  }
+
+  static Future<List<Track>?> gettoptracksUser({String period = 'overall'}) async{
+    List<Track>? toptracks;
+    var userName = await SharedPreference.getUsername();
+    var url = Uri.parse(ApiUrl.buildUrl(Method.gettoptracksUser(userName, period), apiKey));
+    var responsed = await http.get(url);
+    if (responsed.statusCode == 200) {
+      var list = jsonDecode(responsed.body);
+      list = list['toptracks'];
+      toptracks = List<Track>.from((list['track'] as List<dynamic>).map<Track>((x) => Track.fromJson(x as Map<String, dynamic>)));
+      for (var e in toptracks) {            
+        url = Uri.parse(ApiUrl.buildUrl(Method.getinfoTrack(e.artist!, e.name!), apiKey));
+        responsed = await http.get(url);
+        if(responsed.statusCode == 200){
+          var list = jsonDecode(responsed.body);
+          if(list['error'] == null){
+            if(list['track']['album'] != null){
+              var realImage = list['track']['album']['image'][2]['#text'];
+              e.image = realImage;
+            }else{
+              url = Uri.parse(ApiUrl.buildUrl(Method.gettopalbumsArtist(e.name!), apiKey));
+              responsed = await http.get(url);
+              if(responsed.statusCode == 200){
+                var list = jsonDecode(responsed.body);
+                if(list['topalbums'] != null){
+                  var realImage = list['topalbums']['album'][0]['image'][3]['#text'];
+                  e.image = realImage;
+                }
+              }
+            }
+          }              
+        }
+      }
+    }
+    return toptracks;
+  }
+
+  static Future<List<Artist>?> gettopartistsUser({String period = 'overall'}) async{
+    var userName = await SharedPreference.getUsername();
+    List<Artist>? topartists;
+    var url = Uri.parse(ApiUrl.buildUrl(Method.gettopartistsUser(userName, period), apiKey));
+    var responsed = await http.get(url);
+    if (responsed.statusCode == 200) {
+      var list = jsonDecode(responsed.body);
+      list = list['topartists'];
+      topartists = List<Artist>.from((list['artist'] as List<dynamic>).map<Artist>((x) => Artist.fromJson(x as Map<String, dynamic>)));
+      for (var e in topartists) {
+        url = Uri.parse(ApiUrl.buildUrl(Method.gettopalbumsArtist(e.name!), apiKey));
+        responsed = await http.get(url);
+        if(responsed.statusCode == 200){
+          var list = jsonDecode(responsed.body);
+          if(list['topalbums']['album'] != null){
+            var realImage = list['topalbums']['album'][0]['image'][3]['#text'];
+            e.image = realImage;
+          }
+        }
+      }
+    }
+    return topartists;
+  }
+
+  static Future<List<Album>?> gettopalbumsUser({String period = 'overall'}) async{
+    var userName = await SharedPreference.getUsername();
+    var url = Uri.parse(ApiUrl.buildUrl(Method.gettopalbumsUser(userName, period), apiKey));
+    List<Album>? topalbums;
+    var responsed = await http.get(url);
+    if (responsed.statusCode == 200) {
+      var list = jsonDecode(responsed.body);
+      list = list['topalbums'];
+      topalbums = List<Album>.from((list['album'] as List<dynamic>).map<Album>((x) => Album.fromJson(x as Map<String, dynamic>)));
+    } 
+    return topalbums;
   }
 
 }
